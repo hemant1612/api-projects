d611bc632f597940b2b1896ab232a692cf4ba440
hemant singh patwal <hemantsingh1612@gmail.com>
2018-01-08 02:45:46 +0530

    request-header-parser

---
+++ Request-Header-Parser/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+
+app.get("/api/whoami", function (request, response) {
+  console.log();
+  console.log(request.headers['accept-language']);
+  console.log(request.headers['user-agent']);
+
+  var Resobj = {
+    'ip-address' : request.headers['x-forwarded-for'] ? request.headers['x-forwarded-for'].split(',')[0] : 'Not known',
+    'language' : request.headers['accept-language'].split(',')[0],
+    'operating-system':request.headers['user-agent'].split(' ')[2]
+  }
+  response.end(JSON.stringify(Resobj));
+}).listen(8000)
